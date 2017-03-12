82c9822247f4c121265561a727edf61287c26d82
PRAKASH <PRAKASH@DESKTOP-815INA5>
2017-03-12 23:24:44 +0530

    First Git Commit

---
+++ online_career_counseling/src/main/java/com/ocs/hackathon/user/User.java
@@ -0,0 +1,20 @@
+package com.ocs.hackathon.user;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Servlet implementation class User
+ */
+@WebServlet("/user")
+public class User extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public User() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
